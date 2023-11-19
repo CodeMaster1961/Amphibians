7711a29e6214a8b325120c21a7ef779d727df0b6
aynac <omer.aynaci@hva.nl>
2023-11-19 13:48:40 +0100

    interface implemented for success , error or loading

--- app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
@@ -4,6 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.amphibians.model.Amphibians
+
+sealed interface AmphibiansUiState {
+    data class Success(val photos: List<Amphibians>) : AmphibiansUiState
+    object Error : AmphibiansUiState
+    object Loading : AmphibiansUiState
+}
 
 class AmphibiansViewModel : ViewModel() {
     var amphibiansUiState by mutableStateOf("")
